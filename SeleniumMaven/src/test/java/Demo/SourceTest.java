d24d090087fac0414e1405b2b5963ea5d1058cd1
Sneha <dysneha@gmail.com>
2022-09-30 15:44:32 +0530

    Raise PR

--- SeleniumMaven/src/test/java/Demo/SourceTest.java
+++ SeleniumMaven/src/test/java/Demo/SourceTest.java
@@ -5,6 +5,7 @@ public class SourceTest {
     public static void main(String[] args) {
         System.out.println("Test");
         System.out.println("NEw ghgh");
+        System.out.println("New PR TEst");
     }
 
 }
